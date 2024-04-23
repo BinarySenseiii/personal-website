f133afa460ab1c56234e6a378f994b89542efa5f
Faisal Tariq <faisaltariq183@gmail.com>
2024-04-23 16:19:59 +0400

    other: next Js project Init with cleanup

---
+++ src/components/ui/fonts.ts
@@ -0,0 +1,7 @@
+import {Bai_Jamjuree} from 'next/font/google'
+
+export const baiJamjuree = Bai_Jamjuree({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-sans',
+})
