d94292c7d7219e33a3b9158bd99bb2d9d6dd1d9a
Faisal Tariq <faisaltariq183@gmail.com>
2024-05-02 01:22:09 +0400

    fixed: @vercel deployment error

--- next.config.mjs
+++ next.config.mjs
@@ -2,28 +2,28 @@ import {build} from 'velite'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: config => {
-    config.plugins.push(new VeliteWebpackPlugin())
-    return config
-  },
+  // webpack: config => {
+  //   config.plugins.push(new VeliteWebpackPlugin())
+  //   return config
+  // },
 }
 export default nextConfig
 
-class VeliteWebpackPlugin {
-  static started = false
-  constructor(/** @type {import('velite').Options} */ options = {}) {
-    this.options = options
-  }
-  apply(/** @type {import('webpack').Compiler} */ compiler) {
-    // executed three times in nextjs !!!
-    // twice for the server (nodejs / edge runtime) and once for the client
-    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
-      if (VeliteWebpackPlugin.started) return
-      VeliteWebpackPlugin.started = true
-      const dev = compiler.options.mode === 'development'
-      this.options.watch = this.options.watch ?? dev
-      this.options.clean = this.options.clean ?? !dev
-      await build(this.options) // start velite
-    })
-  }
-}
+// class VeliteWebpackPlugin {
+//   static started = false
+//   constructor(/** @type {import('velite').Options} */ options = {}) {
+//     this.options = options
+//   }
+//   apply(/** @type {import('webpack').Compiler} */ compiler) {
+//     // executed three times in nextjs !!!
+//     // twice for the server (nodejs / edge runtime) and once for the client
+//     compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
+//       if (VeliteWebpackPlugin.started) return
+//       VeliteWebpackPlugin.started = true
+//       const dev = compiler.options.mode === 'development'
+//       this.options.watch = this.options.watch ?? dev
+//       this.options.clean = this.options.clean ?? !dev
+//       await build(this.options) // start velite
+//     })
+//   }
+// }
